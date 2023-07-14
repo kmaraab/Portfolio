f99a2fe8aebf4624abfa868df5a028f759d10c30
kmaraab <aboubacarcamara0110@gmail.com>
2023-07-14 15:06:59 +0200

    update build

--- src/router.js
+++ src/router.js
@@ -14,7 +14,7 @@ export const router = createBrowserRouter([
         // element: <ErrorPage />,
       },
       {
-        path: '/',
+        path: '/Portfolio',
         caseSensitive: true,
         element: <Accueil />,
       },
